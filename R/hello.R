0d6d74b1b47278d2fdc7d3c966a9e668c9fcab4b
Akylsu <akylsu@gmail.com>
2020-05-12 21:18:40 +0000

    add forint and get binance price functions

--- R/hello.R
+++ R/hello.R
@@ -1,13 +1,17 @@
-#' Dummy function writing "hello world"
-#' @export
 ## Create the mr R package described above with the forint and get_bitcoin_price functions,
 ## and push to a new repo in your GitHub account, so that you can install the package on any
 ## computer via remotes::install_github. Submit the URL to your GitHub repo in Moodle.
 ## default function for later debugging
+#' Dummy function writing "hello world"
+#' @export
+#' @param retried the number of previously done before the exponential backoff sleep
+#' @importFrom binancer binance_coins_prices
 hello <- function() {
   print("Hello, world!")
 }
-#' get bitcoin price
+##
+##
+#' Accessing the Binance API to get the most recent price of a Bitcoin in USD: a wrapper around the \code(binancer) package.
 #' @export
 get_bitcoin_price <- function(retried = 0) {
   tryCatch(
@@ -15,10 +19,13 @@ get_bitcoin_price <- function(retried = 0) {
     error = function(e){
       Sys.sleep(1+retried^2)
       get_bitcoin_price(retried = 1 + retried)
-    }
+    })
+  }
 
-  )}
+
 #' helper function for Ft format
+#' @param x number
 #' @export
+#' @importFrom scales dollar
 forint <- function(x) {
   dollar(x, prefix = '', suffix = 'Ft')}
